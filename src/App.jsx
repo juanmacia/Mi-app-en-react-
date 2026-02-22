27eddd10daf7026cb32527a62ed274424d39803f
Juan Macías <juanmacias811028@gmail.com>
2026-02-22 11:43:13 -0600

    Mi portafolio React

---
+++ src/App.jsx
@@ -0,0 +1,113 @@
+import { useState, useEffect } from "react"
+import "./App.css"
+
+function App() {
+  const [mostrarFormulario, setMostrarFormulario] = useState(false)
+
+  const [formData, setFormData] = useState({
+    nombre: "",
+    email: "",
+    mensaje: ""
+  })
+
+  const [enviado, setEnviado] = useState(false)
+  const [error, setError] = useState("")
+  const [darkMode, setDarkMode] = useState(false)
+
+  useEffect(() => {
+  if (enviado) {
+    const timer = setTimeout(() => {
+      setEnviado(false)
+    }, 3000)
+
+    return () => clearTimeout(timer)
+  }
+}, [enviado])
+
+  const manejarCambio = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  
+    const manejarEnvio = (e) => {
+  e.preventDefault()
+
+
+
+  if (
+    formData.nombre === "" ||
+    formData.email === "" ||
+    formData.mensaje === ""
+  ) {
+    setError("por favor completa todos los campos")
+    return
+  }
+  setError("")
+  console.log("Formulario enviado:", formData)
+
+  setEnviado(true)
+
+  setFormData({
+    nombre: "",
+    email: "",
+    mensaje: ""
+  
+  })
+} //
+return (
+  <div className={`container ${darkMode ? "dark" : ""}`}>
+    <div className="card">
+
+      <h1>Juan Macías</h1>
+      <p>Desarrollador en formación</p>
+
+      <button onClick={() => setMostrarFormulario(!mostrarFormulario)}>
+        Contactame
+      </button>
+
+     <form
+  onSubmit={manejarEnvio}
+  className={`formulario ${mostrarFormulario ? "activo" : ""}`}
+>
+          <input
+            type="text"
+            name="nombre"
+            placeholder="Tu nombre"
+            value={formData.nombre}
+            onChange={manejarCambio}
+          />
+
+          <input
+            type="email"
+            name="email"
+            placeholder="Tu email"
+            value={formData.email}
+            onChange={manejarCambio}
+          />
+
+          <textarea
+            name="mensaje"
+            placeholder="Mensaje"
+            value={formData.mensaje}
+            onChange={manejarCambio}
+          />
+
+          <button type="submit">Enviar</button>
+          {error && <p className="error">{error}</p>}
+        </form>
+
+        <button onClick={() => setDarkMode(!darkMode)}>
+  {darkMode ? "Modo Claro ☀️" : "Modo Oscuro 🌙"}
+</button>
+      
+
+      {enviado && <p>✅ Mensaje enviado</p>}
+
+    </div>
+  </div>
+)
+} // 
+export default App //
